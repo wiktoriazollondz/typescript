11ba5776d655209f2a6111e8662b07713e3ec39a
zoriaa <w.zollondz.037@studms.ug.edu.pl>
2026-04-14 13:35:02 +0200

    filename changed

---
+++ typicalTasks/palindrome.ts
@@ -0,0 +1,13 @@
+const palindrome = (slowo: string): boolean => {
+    for (let i = 0; i < slowo.length / 2; i++) {  
+        if (slowo[i] !== slowo[slowo.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const isPalindrome = (word: string): boolean => {
+    const reverse = word.split("").reverse().join("");
+    return reverse === word
+}
