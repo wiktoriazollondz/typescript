dab34c8c83f6fedc90ef2b39cc0a2bc3e38f51d8
zoriaa <w.zollondz.037@studms.ug.edu.pl>
2026-04-14 13:55:43 +0200

    + log answers

--- typicalTasks/palindrome.ts
+++ typicalTasks/palindrome.ts
@@ -1,13 +1,18 @@
 const palindrome = (slowo: string): boolean => {
-    for (let i = 0; i < slowo.length / 2; i++) {  
-        if (slowo[i] !== slowo[slowo.length - i - 1]) {
-            return false;
-        }
+  for (let i = 0; i < slowo.length / 2; i++) {
+    if (slowo[i] !== slowo[slowo.length - i - 1]) {
+      return false;
     }
-    return true;
-}
+  }
+  return true;
+};
 
 const isPalindrome = (word: string): boolean => {
-    const reverse = word.split("").reverse().join("");
-    return reverse === word
-}
+  const reverse = word.split("").reverse().join("");
+  return reverse === word;
+};
+
+console.log(palindrome("kajak"));
+console.log(palindrome("kalambury"));
+console.log(isPalindrome("kajak"));
+console.log(isPalindrome("kalambury"));
